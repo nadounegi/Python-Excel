7d59f064cc477875cdbaee1936760e5aa57b708f
nadounegi <it22083027@tsb-yyg.ac.jp>
2024-01-25 12:20:07 +0900

    更新

--- 第九回/黄梦婕-kyuyo.py
+++ 第九回/黄梦婕-kyuyo.py
@@ -1,9 +1,11 @@
 from asyncio.windows_events import INFINITE
 from cProfile import label
 from logging import root
-import symtable
 import tkinter as tk
 import openpyxl as excel
+import win32com.client as com
+import os
+from openpyxl.styles import PatternFill
 
 
 def show_selected(event):
@@ -12,7 +14,7 @@ def show_selected(event):
     selection = lb.curselection()
     if selection:
         index = selection[0]
-        syas = ["相沢　沙希", "生田　真一", "井川　春子", "木村　かえ", "近藤　春香"]
+        syas = ["相沢 沙希", "生田 真一", "井川 春子", "木村 かえ", "近藤 春香"]
         selected_sya = syas[index]
 
 
@@ -21,60 +23,133 @@ def get_selected_sya():
 
 
 def sya_no():
-    syas = ["相沢　沙希", "生田　真一", "井川　春子", "木村　かえ", "近藤　春香"]
+    syas = ["相沢 沙希", "生田 真一", "井川 春子", "木村 かえ", "近藤 春香"]
     for i in range(len(syas)):
         if syas[i] == selected_sya:
-            sya_no = 100 + int(i)
+            sya_no = 101 + int(i)
             i += 1
             return sya_no
 
-
+    
 def caculate_salary():
     # 時間内給与
-    base_slary = hw.get() * wh.get()
-    # 残業給与
-    over_time_salary = hw.get() * oh.get() * 1.2
+    base_slary =int(wh.get()) *int(hw.get())
     # 給与合計
-    total_salary = base_slary + over_time_salary
+    total_salary = base_slary + int(ow.get())
 
-    return base_slary, over_time_salary, total_salary
+    return base_slary, total_salary
 
+info = []
 
 def click_btn():
+    base_salary,total_salary = caculate_salary()
+    sya_number = sya_no()  # 获取sya_no函数的返回值
+
+    info.append({
+'       sya_number': sya_number,
+        'selected_sya': get_selected_sya(),
+        'hw': hw.get(),
+        'wd': wd.get(),
+        'wh': wh.get(),
+        'base_salary': base_salary,
+        'oh': oh.get(),
+        'ow': ow.get(),
+        'total_salary': total_salary
+    })
+
     book = excel.load_workbook("第九回/kyuyo.xlsx")
     sheet_sya = get_selected_sya()
+    sya_bango = sya_no()
 
     if sheet_sya:
-        selectedsheet = book[sheet_sya]
+        selectedsheet = book[str(sya_bango)]
         default_sheet = book["給与一覧"]
 
         # 開始行を取得
         min_row = 3
-        while selectedsheet.cell(row=min_row, column=1).value != None:
+        while selectedsheet.cell(row=min_row, column=2).value != None:
             min_row += 1
 
         default_min_row = 3
-        while default_sheet.cell(row=default_min_row, column=1).value != None:
+        while default_sheet.cell(row=default_min_row, column=2).value != None:
             default_min_row += 1
 
         # データを書き込む
-        selectedsheet.cell(row=min_row, column=1).value = hw.get()
-        default_sheet.cell(row=default_min_row, column=1).value = hw.get()
-        selectedsheet.cell(row=min_row, column=2).value = wd.get()
-        default_sheet.cell(row=default_min_row, column=2).value = wd.get()
-        selectedsheet.cell(row=min_row, column=3).value = wh.get()
-        default_sheet.cell(row=default_min_row, column=3).value = wh.get()
-        selectedsheet.cell(row=min_row, column=4).value = oh.get()
-        default_sheet.cell(row=default_min_row, column=4).value = oh.get()
+        # 社員番号
+        selectedsheet.cell(row=min_row, column=2).value = sya_no()
+        default_sheet.cell(row=default_min_row, column=2).value = sya_no()
+        #社員名
+        selectedsheet.cell(row=min_row, column=3).value = get_selected_sya()
+        default_sheet.cell(row=default_min_row, column=3).value = get_selected_sya()
+        #時給
+        selectedsheet.cell(row=min_row, column=4).value = hw.get()
+        default_sheet.cell(row=default_min_row, column=4).value = hw.get()
+        #勤務日
+        selectedsheet.cell(row=min_row, column=5).value = wd.get()
+        default_sheet.cell(row=default_min_row, column=5).value = wd.get()
+        #勤務時間
+        selectedsheet.cell(row=min_row, column=6).value = wh.get()
+        default_sheet.cell(row=default_min_row, column=6).value = wh.get()
+        #時間内給与
+        base_salary, total_salary = caculate_salary()
+        selectedsheet.cell(row=min_row, column=7).value = base_salary
+        default_sheet.cell(row=default_min_row, column=7).value = base_salary
+        #残業時間
+        overtime_hours = float(oh.get())
+        selectedsheet.cell(row=min_row, column=8).value = overtime_hours
+        default_sheet.cell(row=default_min_row, column=8).value = overtime_hours
+        #残業時間が3時間を超えたら赤色にする
+        if overtime_hours >= 3:
+             red_fill = PatternFill(start_color="FFFF0000", end_color="FFFF0000", fill_type="solid")
+             selectedsheet.cell(row=min_row, column=8).fill = red_fill
+             default_sheet.cell(row=default_min_row, column=8).fill = red_fill
+        #残業代
+        selectedsheet.cell(row=min_row, column=9).value = ow.get()
+        default_sheet.cell(row=default_min_row, column=9).value = ow.get()
+        #給与合計
+        selectedsheet.cell(row=min_row, column=10).value = total_salary
+        default_sheet.cell(row=default_min_row, column=10).value = total_salary
 
         # Excelファイルを保存
         book.save("第九回/kyuyo.xlsx")
 
+def set_worksheet_layout(worksheet):
+    # 设置工作表为横向，xlLandscape 的值为 2
+    worksheet.PageSetup.Orientation = 2
+    # 将内容适应一页宽
+    worksheet.PageSetup.FitToPagesWide = 1
+    # 注释掉 FitToPagesTall 或将其设置为 1
+    # worksheet.PageSetup.FitToPagesTall = 1
 
+def convert_to_pdf(excel_path, pdf_path):
+    excel = com.Dispatch("Excel.Application")
+    excel.Visible = False
+    workbook = None
+
+    try:
+        workbook = excel.Workbooks.Open(excel_path)
+
+        # 遍历所有工作表并应用布局设置
+        for sheet in workbook.Sheets:
+            set_worksheet_layout(sheet)
+
+        workbook.ExportAsFixedFormat(0, pdf_path)
+    except Exception as e:
+        print("转换失败:", e)
+    finally:
+        if workbook is not None:
+            workbook.Close()
+        excel.Quit()
+def on_convert_to_pdf():
+    excel_path = "D:\Python Excel\第九回\kyuyo.xlsx"  # Excel 文件路径
+    pdf_path = "D:\Python Excel\第九回\kyuyo.pdf"    # PDF 文件将被保存的路径
+    convert_to_pdf(excel_path, pdf_path)
+    print("转换完成")
+            
 root = tk.Tk()
 root.title("給与計算")
 #   画面サイズ
-root.geometry("700x700")
+root.geometry("1500x1500")
 
 # 社員リスト
 sl_lb = tk.Label(root, text="社員リスト", font=("New Times Roman", 12, "bold"))
@@ -89,24 +164,41 @@ lb.bind("<<ListboxSelect>>", show_selected)
 
 # 勤務日入力欄
 wd_lb = tk.Label(root, text="勤務日", font=("New Times Roman", 12, "bold"))
-wd_lb.place(x=220, y=300)
+wd_lb.place(x=235, y=210)
 wd = tk.Entry(width=15)
-wd.place(x=280, y=300)
+wd.place(x=330, y=210)
+
+# 時給入力欄
+hw_lb = tk.Label(root, text="時給", font=("New Times Roman", 12, "bold"))
+hw_lb.place(x=235, y=250)
+hw = tk.Entry(width=15)
+hw.place(x=330, y=250)
 
 # 勤務時間入力欄
 wh_lb = tk.Label(root, text="勤務時間", font=("New Times Roman", 12, "bold"))
-wh_lb.place(x=200, y=340)
+wh_lb.place(x=235, y=290)
 wh = tk.Entry(width=15)
-wh.place(x=280, y=340)
+wh.place(x=330, y=290)
 
 # 残業時間入力欄
 oh_lb = tk.Label(root, text="残業時間", font=("New Times Roman", 12, "bold"))
-oh_lb.place(x=200, y=380)
+oh_lb.place(x=235, y=330)
 oh = tk.Entry(width=15)
-oh.place(x=280, y=380)
+oh.place(x=330, y=330)
+
+#残業代入力欄
+ow_lb = tk.Label(root, text="残業代", font=("New Times Roman", 12, "bold"))
+ow_lb.place(x=235, y=370)
+ow = tk.Entry(width=15)
+ow.place(x=330, y=370)
+
+
 
 # 入力ボタン
-btn = tk.Button(root, text="入力", font=("New Times Roman", 12, "bold"))
+btn = tk.Button(root, text="入力", font=("New Times Roman", 12, "bold"), command=click_btn)
 # ボタンの位置を指定
-btn.place(x=280, y=450)
+btn.place(x=235, y=450)
+
+convert_pdf_btn = tk.Button(root, text="PDFファイルに転換", command=on_convert_to_pdf)
+convert_pdf_btn.place(x=450, y=450)  # 调整按钮位置
 root.mainloop()
--- 第五回/slot2.py
+++ 第五回/slot2.py
@@ -1,15 +1,19 @@
-from logging import root
 import openpyxl as excel
 import tkinter
 import random
 
+book = excel.Workbook()
+sheet = book.active
+sheet["b4"] = "結果"
 
+
+# Initialize the variables
 s1, s2, s3 = 0, 0, 0
 
 
 def click_btn1():
     global r1, s1
-    r1 = random.randint(1, 9)
+    r1 = random.randint(1, 3)
     label1["text"] = r1
     label1.update()
     s1 = 1
@@ -18,7 +22,7 @@ def click_btn1():
 
 def click_btn2():
     global r2, s2
-    r2 = random.randint(1, 9)
+    r2 = random.randint(1, 3)
     label2["text"] = r2
     label2.update()
     s2 = 1
@@ -27,7 +31,7 @@ def click_btn2():
 
 def click_btn3():
     global r3, s3
-    r3 = random.randint(1, 9)
+    r3 = random.randint(1, 3)
     label3["text"] = r3
     label3.update()
     s3 = 1
@@ -36,29 +40,42 @@ def click_btn3():
 
 def click_btn4():
     global s1, s2, s3
-    label1["text"] = "?"
-    label2["text"] = "?"
-    label3["text"] = "?"
-    lbkekka["text"] = "?"
+    label1["text"] = ""
+    label2["text"] = ""
+    label3["text"] = ""
+    lbkekka["text"] = ""
+    s1 = 0
+    s2 = 0
+    s3 = 0
 
 
 def kekka():
     global s1, s2, s3, r1, r2, r3
-    if s1 and s2 and s3:
-        if r1 == r2 == r3:
+    if s1 == 1 and s2 == 1 and s3 == 1:
+        if r1 == r2 and r2 == r3:
             lbkekka["text"] = "あたり!"
         else:
             lbkekka["text"] = "はずれ!"
+    global count
+    count = 4
 
 
-def write_to_excel():
-    pass
+def click_btn5():
+    global count
+    count += 1
+    sheet.cell(row=count, column=2, value=r1)
+    sheet.cell(row=count, column=3, value=r2)
+    sheet.cell(row=count, column=4, value=r3)
+    sheet.cell(row=count, column=5, value=lbkekka["text"])
+
+    book.save("第五回/slot.xlsx")
+    print("保存完了")
 
 
 root = tkinter.Tk()
 root.title("slot")
 root.resizable(False, False)
-root.geometry("400x400")
+root.geometry("400x600")
 
 label1 = tkinter.Label(root, text="?", font=("Times New Roman", 20), bg="white")
 label1.place(x=50, y=80)
@@ -91,5 +108,10 @@ btnclear = tkinter.Button(
     root, text="クリア", font=("Times New Roman", 20), command=click_btn4, fg="skyblue"
 )
 btnclear.place(x=150, y=350)
+
+btnsave = tkinter.Button(
+    root, text="保存", font=("Times New Roman", 20), command=click_btn5, fg="skyblue"
+)
+btnsave.place(x=150, y=450)
 
 root.mainloop()
